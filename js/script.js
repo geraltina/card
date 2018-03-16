1f18334368827363a81c124120c9bfdad5f765e5
geraltina <evgpestr@yandex.ru>
2018-03-16 19:32:19 +0300

    Оплата, разметка и стили

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var navMenu = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
+var pageHeader = document.querySelector(".page-header__nav-wrapper");
+
+navMenu.classList.remove("main-nav--nojs");
+
+navToggle.addEventListener("click", function (evt) {
+  if (navMenu.classList.contains("main-nav--closed")) {
+    navMenu.classList.remove("main-nav--closed");
+    navMenu.classList.add("main-nav--opened");
+    pageHeader.classList.add("page-header__nav-wrapper--opened");
+  } else {
+    navMenu.classList.remove("main-nav--opened");
+    navMenu.classList.add("main-nav--closed");
+    pageHeader.classList.remove("page-header__nav-wrapper--opened");
+  }
+});
